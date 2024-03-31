058888ce82f05ae00b953175d91d98c2a7e94a94
pajon1 <p.pajon@udc.es>
2024-03-31 17:30:33 +0200

    Primera Iteracion Completa

--- code.jl
+++ code.jl
@@ -213,11 +213,11 @@ function procesar_archivos(input_folder::String)
     println("SVM MSE:",mse)
     println("SVM MAE:",mae)
     mse,mae=entrenar_tree(input, target)
-    println("SVM TREE:",mse)
-    println("SVM TREE:",mae)
+    println("TREE MSE:",mse)
+    println("TREE MAE:",mae)
     mse,mae=entrenar_KNe(input, target)
-    println("SVM KNe:",mse)
-    println("SVM KNe:",mae)
+    println("KNe MSE:",mse)
+    println("KNe MAE:",mae)
 end
 
 function entrenar_RRNNAA(input_train,target_train)
@@ -318,7 +318,16 @@ function entrenar_RRNNAA(input_train,target_train)
     p1=plot(historico_train, title="Historico Train", subplot=1)
     p2=plot(historico_test, title="Historico Test", subplot=1)
     p3=plot(historico_validation, title="Historico Validation", subplot=1)
-    display(plot(p1,p2,p3, layout = (3,1)));
+    display(plot(p1,p2,p3, layout = (3,1)));   
+    println(typeof(target_test))
+    # Convertir los valores en bool dependiendo si son mayores o menores que 0.5
+    outputP = Array{Bool}(outputP .> 0.5)
+    outputP = [elemento for fila in outputP for elemento in fila]
+    # Convertir los valores en bool dependiendo si son mayores o menores que 0.5
+    target_test = Array{Bool}(target_test .> 0.5)
+    target_test = [elemento for fila in target_test for elemento in fila]
+    println(typeof(outputP))
+    printConfusionMatrix(outputP,target_test)
 
     return vlose
 end
@@ -354,15 +363,13 @@ function entrenar_svm(input_train, target_train)
     #aux=crear_vector(target_test)'
     #println(aux)
     #printConfusionMatrix(testOutputs,collect(aux))
+    aux=crear_vector(target_test)'
+    printConfusionMatrix(recrear_vector(testOutputs,output_length),recrear_vector(aux,output_length))
     # Calcular el error cuadrático medio (MSE)
     mse = mean((testOutputs .- target_test').^2)
     # Calcular el error absoluto medio (MAE)
     mae = mean(abs.(testOutputs .- target_test'))
-    return mse,mae
-end
-
-function comparar_resultados()
-    
+    return mse,mae    
 end
 
 function entrenar_tree(input_train, target_train)
@@ -393,6 +400,8 @@ function entrenar_tree(input_train, target_train)
     model = DecisionTreeClassifier(max_depth=4, random_state=1)
     fit!(model, input_train', crear_vector(target_train)');
     testOutputs = predict(model, input_test');
+    aux=crear_vector(target_test)'
+    printConfusionMatrix(recrear_vector(testOutputs,output_length),recrear_vector(aux,output_length))
     # Calcular el error cuadrático medio (MSE)
     mse = mean((testOutputs .- target_test').^2)
     # Calcular el error absoluto medio (MAE)
@@ -428,6 +437,8 @@ function entrenar_KNe(input_train, target_train)
     model = KNeighborsClassifier(3);
     fit!(model, input_train', crear_vector(target_train)');
     testOutputs = predict(model, input_test');
+    aux=crear_vector(target_test)'
+    printConfusionMatrix(recrear_vector(testOutputs,output_length),recrear_vector(aux,output_length))
     # Calcular el error cuadrático medio (MSE)
     mse = mean((testOutputs .- target_test').^2)
     # Calcular el error absoluto medio (MAE)
@@ -448,6 +459,14 @@ function crear_vector(matriz::Matrix{Float64})
     end
     
     return vector_resultado
+end
+
+function recrear_vector(array,size::Int)
+    result=zeros(length(array)*size)
+    for i in length(array)
+        result[(i-1)*size+(array[i]-1)]=1
+    end
+    return Array{Bool}(result .> 0.5)
 end
 
 function FFT_data(input_wav1::String, value::Int)
@@ -548,6 +567,90 @@ function mirar2notas(input::Matrix{Float64},frecuencia::Float32,note1::Int, note
     #println("Desv tipica de la señal en frecuencia entre $(f1) y $(f2) Hz: ", std(senalFrecuencia[m1:m2]));
     return mean(senalFrecuencia[m1:m2]),std(senalFrecuencia[m1:m2])
 end
+
+#CODIGO COPY 
+
+accuracy(targets::AbstractArray{Bool,1}, outputs::AbstractArray{Bool,1}) = mean(outputs .== targets);
+
+function confusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+    @assert(length(outputs) == length(targets))
+    # Para calcular la precision y la tasa de error, se puede llamar a las 
+    #funciones definidas en la practica 2
+    acc = accuracy(outputs, targets) # Precision, definida previamente 
+    #en una practica anterior
+    errorRate = 1.0 - acc
+    recall = mean(outputs[targets]) # Sensibilidad
+    specificity = mean(.!outputs[.!targets]) # Especificidad
+    precision = mean(targets[outputs]) # Valor predictivo positivo
+    NPV = mean(.!targets[.!outputs]) # Valor predictivo negativo
+    # Controlamos que algunos casos pueden ser NaN
+    # Para el caso de sensibilidad y especificidad, en un conjunto de 
+    #entrenamiento estos no pueden ser NaN, porque esto indicaria que se ha intentado
+    #entrenar con una unica clase
+    # Sin embargo, sí pueden ser NaN en el caso de aplicar un modelo en un 
+    #conjunto de test, si este sólo tiene patrones de una clase
+    # Para VPP y VPN, sí pueden ser NaN en caso de que el clasificador lo haya 
+    #clasificado todo como negativo o positivo respectivamente
+    # En estos casos, estas metricas habria que dejarlas a NaN para indicar que 
+    #no se han podido evaluar
+    # Sin embargo, como es posible que se quiera combinar estos valores al 
+    #evaluar una clasificacion multiclase, es necesario asignarles un valor. El 
+    #criterio que se usa aqui es que estos valores seran igual a 0
+    # Ademas, hay un caso especial: cuando los VP son el 100% de los patrones, o
+    #los VN son el 100% de los patrones
+
+    # En este caso, el sistema ha actuado correctamente, así que controlamos 
+    #primero este caso
+    if isnan(recall) && isnan(precision) # Los VN son el 100% de los patrones
+        recall = 1.0
+        precision = 1.0
+    elseif isnan(specificity) && isnan(NPV) # Los VP son el 100% de los patrones
+        specificity = 1.0
+        NPV = 1.0
+    end
+    # Ahora controlamos los casos en los que no se han podido evaluar las 
+    #metricas excluyendo los casos anteriores
+    recall = isnan(recall) ? 0.0 : recall
+    specificity = isnan(specificity) ? 0.0 : specificity
+    precision = isnan(precision) ? 0.0 : precision
+    NPV = isnan(NPV) ? 0.0 : NPV
+    # Calculamos F1, teniendo en cuenta que si sensibilidad o VPP es NaN (pero 
+    #no ambos), el resultado tiene que ser 0 porque si sensibilidad=NaN entonces 
+    #VPP=0 y viceversa
+    F1 = (recall == precision == 0.0) ? 0.0 :
+         2 * (recall * precision) / (recall + precision)
+    # Reservamos memoria para la matriz de confusion
+    confMatrix = Array{Int64,2}(undef, 2, 2)
+    # Ponemos en las filas los que pertenecen a cada clase (targets) y en las 
+    #columnas los clasificados (outputs)
+    # Primera fila/columna: negativos
+    # Segunda fila/columna: positivos
+    # Primera fila: patrones de clase negativo, clasificados como negativos o 
+    #positivos
+    confMatrix[1, 1] = sum(.!targets .& .!outputs) # VN
+    confMatrix[1, 2] = sum(.!targets .& outputs) # FP
+    # Segunda fila: patrones de clase positiva, clasificados como negativos o 
+    #positivos
+    confMatrix[2, 1] = sum(targets .& .!outputs) # FN
+    confMatrix[2, 2] = sum(targets .& outputs) # VP
+    return (acc, errorRate, recall, specificity, precision, NPV, F1, confMatrix)
+end;
+
+function printConfusionMatrix(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+    (acc, errorRate, recall, specificity, precision, NPV, F1, confMatrix) = confusionMatrix(outputs, targets)
+
+    println("Confusion Matrix:")
+    println(confMatrix)
+    println("Accuracy: $acc")
+    println("Error Rate: $errorRate")
+    println("Recall (Sensitivity): $recall")
+    println("Specificity: $specificity")
+    println("Precision (Positive Predictive Value): $precision")
+    println("Negative Predictive Value: $NPV")
+    println("F1 Score: $F1")
+    println(" ")
+end
+#fin CODIGO COPY
 
 
 #canales, Fs, tasa_bits_codificacion,tamano = obtener_info_wav("train_data/01.wav")
